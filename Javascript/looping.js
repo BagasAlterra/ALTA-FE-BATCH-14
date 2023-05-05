4131debdd6b2a1f95eee5f3d5dc19f5081d5ccd0
BagasAlterra <bagas.dhitya@alterra.id>
2023-05-05 13:21:54 +0700

    added for and while looping

---
+++ Javascript/looping.js
@@ -0,0 +1,90 @@
+// --- Jenis jenis looping di javascript :
+// 1. for loop
+// 2. while loop
+// 3. for in
+// 4. for of
+// 5. forEach
+// 6. map -> akan sering digunakan di FE
+
+// For Loop : digunakan ketika kondisi awal, iterasi sudah jelas -> looping berdasarkan kondisi awal, iterasi, dan increment/decrement
+// While Loop : digunakan ketika kondisi awal, iterasi belum jelas -> looping berdasarkan kondisi saja
+
+// ------- FOR LOOP -------
+
+// -- Kerangka For Loop
+// for( init; condition; post){
+
+// }
+
+// --- SINGLE LOOP
+function forLoopIncrement(number) {
+  for (let i = 0; i <= number; i++) {
+    // console.log("result of increment : ", i);
+  }
+}
+
+function forLoopDecrement(number) {
+  for (let i = 0; i >= number; i--) {
+    console.log("result of decrement : ", i);
+  }
+}
+
+function checkGanjilGenap(number) {
+  for (let i = 0; i <= number; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " adalah Bilangan Genap");
+    } else {
+      console.log(i + " adalah Bilangan Ganjil");
+    }
+  }
+}
+
+// forLoopIncrement(10);
+// forLoopDecrement(-5);
+// checkGanjilGenap(10);
+// checkGanjilGenap(30);
+
+// --- NESTED FOR LOOP
+// --- cara kerja nested for loop berurutan
+// --- mulai dari for ke 1, for ke 2, dst.
+// --- for ke 2 dipengaruhi dari for sebelumnya, for ke 3 dipengaruhi dari for ke 2, dst.
+
+function nestedForLoop(number) {
+  for (let i = 0; i < number; i++) {
+    console.log("Main looping : ", i);
+    for (let j = 0; j < number; j++) {
+      console.log("Sub looping : ", j);
+    }
+  }
+}
+
+// nestedForLoop(5);
+
+// ------- WHILE LOOP -------
+// --- cara kerja while loop itu berdasarkan kondisinya
+
+function whileLoop(end, start) {
+  while (start <= end) {
+    console.log("start", start);
+    start++;
+  }
+}
+
+// whileLoop(20, 5);
+
+// --- NESTED WHILE LOOP
+
+function nestedWhileLoop(number) {
+  let i = 0;
+  while (i < number) {
+    console.log("Main Looping : ", i);
+    let j = 0;
+    while (j < number) {
+      console.log("Sub Looping : ", j);
+      j++;
+    }
+    i++;
+  }
+}
+
+nestedWhileLoop(5);
