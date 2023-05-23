c9e6849af80fdb572b459188cb5d92df1b2d3f31
BagasAlterra <bagas.dhitya@alterra.id>
2023-05-23 14:19:58 +0700

    add new dummy

--- Javascript/react/first-react-vite/src/App.tsx
+++ Javascript/react/first-react-vite/src/App.tsx
@@ -1,3 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
+
 // ================================= CONTOH STATE PADA DARK MODE =================================
 
 // import { Component } from "react";
