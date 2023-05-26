f8f93591a4f166fbef9f6509ef83a70eb03de106
BagasAlterra <bagas.dhitya@alterra.id>
2023-05-26 09:53:16 +0700

    added fake credentials for login

--- Javascript/react/first-react-hooks/src/pages/home/index.tsx
+++ Javascript/react/first-react-hooks/src/pages/home/index.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react"
+
+import Button from "../../components/Button"
 
 const Home = () => {
+
+    const [count, setCount] = useState<number>(0)
+
+    const getIncrement = () => {
+        setCount(count + 1)
+    }
+
+    const getDecrement = () => {
+        setCount(count - 1)
+    }
+
     return (
-        <div>Home</div>
+        <div className="w-screen bg-white">
+            <h1>Result : {count}</h1>
+            <div className="w-20 h-12 flex flex-column">
+                <Button
+                    id="increment"
+                    label="+"
+                    onClick={() => getIncrement()}
+                />
+                <Button
+                    id="decrement"
+                    label="-"
+                    onClick={() => getDecrement()}
+                />
+            </div>
+        </div>
     )
 }
 
--- Javascript/react/first-react-hooks/src/pages/login/index.tsx
+++ Javascript/react/first-react-hooks/src/pages/login/index.tsx
@@ -1,7 +1,55 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Swal from "sweetalert2"
+
+import Input from "../../components/Input"
+import Button from "../../components/Button"
+
+import { data } from "../../dummy/credentials.json"
 
 const Login = () => {
+
+    const navigate = useNavigate()
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string | number>("")
+
+    const handleLogin = () => {
+
+        if (username === data[0].username && password == data[0].password) {
+            navigate("/home")
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Login Failed",
+                text: "Fill your username and password"
+            })
+        }
+    }
+
     return (
-        <div>Login</div>
+        <div className="min-h-screen w-screen bg-gray-100 flex items-center justify-center">
+            <div className="bg-white p-6 rounded-md shadow-md">
+                <Input
+                    id="username"
+                    name="username"
+                    value={username}
+                    label="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <Input
+                    id="password"
+                    name="password"
+                    value={password}
+                    label="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button
+                    id="login"
+                    label="Login"
+                    onClick={() => handleLogin()}
+                />
+            </div>
+        </div>
     )
 }
 
