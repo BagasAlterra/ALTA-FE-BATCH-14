693a77391059b8e5fdefb42f0784d314e2c256bc
BagasAlterra <bagas.dhitya@alterra.id>
2023-05-30 10:27:42 +0700

    create redux for auth and counter

--- Javascript/react/first-react-redux/src/pages/count/index.tsx
+++ Javascript/react/first-react-redux/src/pages/count/index.tsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
+import Layout from "../../components/Layout";
+import Button from "../../components/Button";
+import {
+  CounterState,
+  increment,
+  decrement,
+} from "../../features/counterSlice";
+import { AuthState } from "../../features/authSlice";
+
 const Count = () => {
-  return <div>Count</div>;
+  const dispatch = useDispatch();
+  const counter = useSelector(
+    (state: { counter: CounterState }) => state.counter
+  );
+  const auth = useSelector((state: { auth: AuthState }) => state.auth);
+
+  return (
+    <Layout>
+      <div className="m-10 space-y-5">
+        <h1 className="text-black">
+          {auth.isAuthenticated === true
+            ? `Hello, welcome ${auth.user?.username}`
+            : "You're not logged in"}
+        </h1>
+        <div className="font-semibold text-black">
+          <p>Result : {counter?.value}</p>
+        </div>
+        <div className="space-x-5 flex flex-row w-40 h-12">
+          <Button
+            id="increment"
+            label="+"
+            onClick={() => dispatch(increment())}
+          />
+          <Button
+            id="decrement"
+            label="-"
+            onClick={() => dispatch(decrement())}
+          />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Count;
--- Javascript/react/first-react-redux/src/pages/login/index.tsx
+++ Javascript/react/first-react-redux/src/pages/login/index.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { User, login } from "../../features/authSlice";
+import { useDispatch } from "react-redux";
+
+import Swal from "sweetalert2";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string | number>();
+
+  const onLogin = () => {
+    const user: User = {
+      username,
+      password,
+    };
+
+    if (user.username !== "" && user.password !== "") {
+      dispatch(login(user));
+      navigate("/count");
+    } else {
+      Swal.fire({
+        title: "Failed",
+        text: "Please check your username or password!",
+        confirmButtonText: "OK",
+      });
+    }
+  };
+
+  return (
+    <div className="min-h-screen w-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-md shadow-md">
+        <Input
+          id="username"
+          name="username"
+          label="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button id="login" label="Login" onClick={() => onLogin()} />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
