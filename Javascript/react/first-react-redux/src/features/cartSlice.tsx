30708a7472fcef194d766f10f0814400b72a2cab
BagasAlterra <bagas.dhitya@alterra.id>
2023-05-30 10:51:33 +0700

    add slice and store for cart

---
+++ Javascript/react/first-react-redux/src/features/cartSlice.tsx
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface Item {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export interface CartState {
+  items: Item[];
+}
+
+const initialState: CartState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action: PayloadAction<Item>) {
+      state.items.push(action.payload);
+    },
+    removeItem(state, action: PayloadAction<number>) {
+      state.items = state.items.filter((item) => {
+        item.id !== action.payload;
+      });
+    },
+  },
+});
+
+export const { addItem, removeItem } = cartSlice.actions;
